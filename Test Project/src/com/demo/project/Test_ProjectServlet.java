8ad9db9d52d74475dbd5df9ab619b860736b18cb
karthick <karthick@full226-prt.full.creative>
2018-12-06 16:16:53 +0530

    Initial commit

---
+++ Test Project/src/com/demo/project/Test_ProjectServlet.java
@@ -0,0 +1,12 @@
+package com.demo.project;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Test_ProjectServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
